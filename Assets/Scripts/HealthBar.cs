a3188de43697d73a4d35c6419e149816f0dc77f0
Gaudham P <gaudhamp@gmail.com>
2020-03-14 14:34:29 +0530

    Created a health bar UI element

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    Transform mainCameraTransform;
+
+    public Slider healthSlider;
+
+    IPawn attachedParent;
+
+    void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+
+        attachedParent = GetComponentInParent<IPawn>();
+
+        healthSlider.minValue = 0;
+
+        healthSlider.maxValue = attachedParent.MaxHealth;
+    }
+
+    void Update()
+    {
+        healthSlider.value = attachedParent.MaxHealth;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + mainCameraTransform.forward);
+    }
+}
