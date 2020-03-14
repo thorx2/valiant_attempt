1ff0587ec788db44681a31a8a601303944f98783
Gaudham P <gaudhamp@gmail.com>
2020-03-14 14:35:24 +0530

    Add game manger and spawn logic for enemies

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public enum EGameState
+    {
+        GameMenu,
+        BoostMenu,
+        LevelComplete,
+        LevelFail
+    }
+
+    public SpawnPointModel[] SpawnPoints;
+
+    public EGameState ActiveGameState = EGameState.GameMenu;
+    int CurrentLevel = 1;
+
+    public GameObject EnemyPrefab;
+
+    public GameObject PlayerObject;
+
+    public Transform PlayerSpawnPoint;
+    
+    void Start()
+    {
+        SpawnEnemies();
+
+        PlayerObject.transform.position = PlayerSpawnPoint.position;
+    }
+
+    void SpawnEnemies()
+    {
+        int spawnCount = 0;
+        while(spawnCount != CurrentLevel + 1)
+        {
+            System.Random randGen = new System.Random();
+            int currentPos = randGen.Next(1, 8);
+            if (!SpawnPoints[currentPos].IsUsed)
+            {
+                Instantiate(EnemyPrefab, SpawnPoints[currentPos].SpawnPoint.position, SpawnPoints[currentPos].SpawnPoint.rotation);
+                spawnCount++;
+            }
+        }
+    }
+
+    void Update()
+    {
+        switch (ActiveGameState)
+        {
+            case EGameState.LevelFail:
+            
+                break;
+
+            case EGameState.LevelComplete:
+                
+                foreach(var spawnPoint in SpawnPoints)
+                {
+                    spawnPoint.IsUsed = false;
+                }
+
+                ++CurrentLevel;
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPointModel.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class SpawnPointModel
+{
+    public bool IsUsed = false;
+    public Transform SpawnPoint;
+}
