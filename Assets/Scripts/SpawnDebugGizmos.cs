4dcdb5ddb36d54826b5ee61c187ab975dc679f3a
Gaudham P <gaudhamp@gmail.com>
2020-03-18 12:17:45 +0530

    Add need coin texture

---
+++ Assets/Scripts/SpawnDebugGizmos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class SpawnDebugGizmos : MonoBehaviour
+{
+    Transform[] children;
+    // Start is called before the first frame update
+    void Start()
+    {
+        children = transform.GetComponentsInChildren<Transform>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach(var tran in children)
+        {
+            if (tran != transform)
+                Gizmos.DrawSphere(tran.position, 0.5f);
+        }
+    }
+}
